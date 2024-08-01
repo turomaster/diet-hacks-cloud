389f638319de861a3a8468a21567dc8e676d2f8e
turomaster <turoneska1111@gmail.com>
2024-08-01 17:03:23 +0000

    Fixed key on list item to be unique with category.id

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -24,7 +24,7 @@ export function NavBar({ categories }: Props) {
             <li className="mb-2 font-bold">Categories</li>
             {categories.map((category, index) => (
               <li
-                key={category.name + index}
+                key={category.id + index}
                 className="rounded-lg mx-4 mb-4 hover:bg-gray-200">
                 <Link to="#">{category.name}</Link>
               </li>
