70387f6917f1d0f60e5a24c2ce85445a32c9271e
turomaster <turoneska1111@gmail.com>
2024-08-13 21:12:38 +0000

    Added downvote logic to downvote icon and decrementing totalVotes if downvote is clicked

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { PiArrowFatUp } from 'react-icons/pi';
+import { PiArrowFatDownFill, PiArrowFatUp } from 'react-icons/pi';
 import { PiArrowFatUpFill } from 'react-icons/pi';
 import { PiArrowFatDown } from 'react-icons/pi';
 import { Link } from 'react-router-dom';
@@ -11,17 +11,31 @@ type Props = {
 };
 
 export function Card({ post }: Props) {
-  const { postVotes, handleViews, handleUpvote } = usePosts();
+  const { postVotes, handleViews, handleUpvote, handleDownvote } = usePosts();
   const { user } = useUser();
   let totalVotes = 0;
 
-  const result = postVotes?.find(
-    (vote) => vote.postId === post.postId && user?.userId === vote.userId
+  const upvoteResult = postVotes?.find(
+    (vote) =>
+      vote.postId === post.postId &&
+      user?.userId === vote.userId &&
+      vote.voteType === 'upvote'
+  );
+
+  const downvoteResult = postVotes?.find(
+    (vote) =>
+      vote.postId === post.postId &&
+      user?.userId === vote.userId &&
+      vote.voteType === 'downvote'
   );
 
   postVotes?.map((vote) => {
-    if (vote.postId === post.postId && vote.voteType === 'upvote') {
-      totalVotes++;
+    if (vote.postId === post.postId) {
+      if (vote.voteType === 'upvote') {
+        totalVotes++;
+      } else if (vote.voteType === 'downvote' && totalVotes > 0) {
+        totalVotes--;
+      }
     }
   });
 
@@ -39,20 +53,31 @@ export function Card({ post }: Props) {
       <div className="card-footer flex flex-wrap mt-4">
         <div className="flex basis-full justify-between">
           <div className="vote-actions flex items-center">
-            {result && (
+            {upvoteResult && (
               <PiArrowFatUpFill
                 className="cursor-pointer text-2xl"
                 onClick={() => handleUpvote(post.postId)}
               />
             )}
-            {!result && (
+            {!upvoteResult && (
               <PiArrowFatUp
                 className="cursor-pointer text-2xl"
                 onClick={() => handleUpvote(post.postId)}
               />
             )}
             <div className="total-votes px-2">{totalVotes}</div>
-            <PiArrowFatDown className="cursor-pointer text-2xl" />
+            {downvoteResult && (
+              <PiArrowFatDownFill
+                className="cursor-pointer text-2xl"
+                onClick={() => handleDownvote(post.postId)}
+              />
+            )}
+            {!downvoteResult && (
+              <PiArrowFatDown
+                className="cursor-pointer text-2xl"
+                onClick={() => handleDownvote(post.postId)}
+              />
+            )}
           </div>
         </div>
         <div className="basis-full pt-2">
